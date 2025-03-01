ad9aeabe8e2330dd7616b04197c583aaddb794db
AndrewGlasgow <lord.rayleigh@gmail.com>
2025-02-28 18:57:42 -0700

    jwebsec.JavaUtils v0.2.0
    Added nullSafeCompare() method.

--- src/main/java/jwebsec/JavaUtils.java
+++ src/main/java/jwebsec/JavaUtils.java
@@ -6,10 +6,43 @@ package jwebsec;
  *   &copy;2025 jWebSec. All rights reserved.
  * </p>
  * 
- * @version 0.1.0
+ * @version 0.2.0
  * @author <a href="mailto:andrew_glasgow.dev@outlook.com">Andrew Glasgow</a>
  */
 public final class JavaUtils {
+    
+    /**
+     * This method performs a null safe comparison of two
+     * <code>Comparable</code> object references, fulfilling the general
+     * contract of the <code>Comparable.compareTo()</code> method as if invoking
+     * <code>ref1.compareTo(ref2)</code>, without the possibility of throwing a
+     * <code>NullPointerException</code>.
+     * <p>
+     *   This method returns:
+     * </p>
+     * <ul>
+     *   <li>-1 if (ref1 != null &amp;&amp; ref2 == null) ||
+     *     (ref1.compareTo(ref2) &lt; 0)</li>
+     *   <li>0 if ref1 == ref2 || ref1.equals(ref2)</li>
+     *   <li>1 if (ref1 == null &amp;&amp; ref != null) ||
+     *     (ref1.compareTo(ref2) &gt; 0)</li>
+     * </ul>
+     * 
+     * @param ref1 first comparable object reference for comparison
+     * @param ref2 seconds comparable object reference for comparison
+     * @return comparison result
+     */
+    public static int nullSafeCompare(Comparable ref1, Comparable ref2) {
+        int result;
+        if (ref1 == ref2) {
+            result = 0;
+        } else if (ref1 == null) {
+            result = 1;
+        } else {
+            result = ref1.compareTo(ref2);
+        }
+        return result;
+    }
     
     /**
      * Returns true if two object references are equal, as defined by the
